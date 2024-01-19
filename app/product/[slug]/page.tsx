e0c4021bff655e1959611d5237e5e56ab92d8672
arafel71 <olbergeron@yahoo.fr>
2024-01-19 16:46:15 +0100

    Add files via upload

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import AddToBag from "@/app/components/AddToBag";
+import CheckoutNow from "@/app/components/CheckoutNow";
 import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { Button } from "@/components/ui/button";
@@ -70,7 +71,9 @@ export default async function ProductPage({
 
                         <div className="flex gap-2.5">
                             <AddToBag currency="USD" description={data.description} image={data.images[0]} name={data.name} price={data.price} key={data._id} price_id={data.price_id}/>
-                            <Button variant={"secondary"}>Checkout Now</Button>
+
+                            <CheckoutNow currency="USD" description={data.description} image={data.images[0]} name={data.name} price={data.price} key={data._id} price_id={data.price_id}/>
+                           
                         </div>
 
                         <p className="mt-12 text-base text-gray-500 tracking-wide">
