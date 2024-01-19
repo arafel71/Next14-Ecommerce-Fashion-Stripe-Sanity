0dce5c484f00c852ee5553a04a841388dd884c08
arafel71 <olbergeron@yahoo.fr>
2024-01-19 19:24:59 +0100

    Add files via upload

--- app/components/Providers.tsx
+++ app/components/Providers.tsx
@@ -4,11 +4,14 @@ import { ReactNode } from "react";
 import { CartProvider as USCProvider } from "use-shopping-cart";
 
 export default function CartProvider({ children }: { children: ReactNode }) {
+
+  const stripestring = process.env.NEXT_PUBLIC_STRIPE_KEY as string;
+
   return (
     <USCProvider
       mode="payment"
       cartMode="client-only"
-      stripe={process.env.NEXT_PUBLIC_STRIPE_KEY as string}
+      stripe={stripestring}
       successUrl="http://keen-seahorse-68531d.netlify.app/stripe/success"
       cancelUrl="http://keen-seahorse-68531d.netlify.app/stripe/error"
       currency="USD"
